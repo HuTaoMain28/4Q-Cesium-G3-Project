a8c22fe8b033f53aee609cd2e7aba0c048bb8d5d
HuTaoMain28 <jgsbalane22@src.pshs.edu.ph>
2025-04-23 09:52:58 +0800

    Create game.js

---
+++ JS/game.js
@@ -0,0 +1,34 @@
+var Boss[5] = [Solmara, Litharos, Pyrrhok, Noctivoros, Mirexa, Varzhul];
+var playerHealth = 20;
+var playerfightRNG = (Math.random() * 10 );
+var playerhealRNG = (Math.random() * 5 );
+var bossfightRNG = (Math.random() * 3 );
+var bosshealRNG = (Math.random() * 10 );
+for (int i=0;i>=5;i++){
+  Boss[i]=
+}
+
+function fight(){
+    
+}
+function heal(){
+
+}
+function solmaraMechanics(){
+  
+}
+function litharosMechanics(){
+
+}
+function pyrrhokMechanics(){
+    
+}
+function noctivorosMechanics(){
+
+}
+function mirexaMechanics(){
+
+}
+function varzhulMechanics(){
+    
+}
